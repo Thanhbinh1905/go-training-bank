b56b9b7f912cd320209f59914da412751356fc42
Thanhbinh1905 <thanhbinh2003195@gmail.com>
2025-09-02 10:11:57 +0000

    load secret and save to .env

--- internal/config/config.go
+++ internal/config/config.go
@@ -8,9 +8,9 @@ import (
 
 type Config struct {
 	DatabaseURL         string        `mapstructure:"DB_SOURCE"`
-	Production          bool          `mapstructure:"PRODUCTION"`
 	TokenSymmetricKey   string        `mapstructure:"TOKEN_SYMMETRIC_KEY"`
 	AccessTokenDuration time.Duration `mapstructure:"ACCESS_TOKEN_DURATION"`
+	Production          bool
 }
 
 func LoadConfig(path string) (*Config, error) {
@@ -20,6 +20,8 @@ func LoadConfig(path string) (*Config, error) {
 	viper.SetConfigName(".env")
 	viper.SetConfigType("env")
 	viper.AutomaticEnv()
+
+	config.Production = false
 
 	if err := viper.ReadInConfig(); err != nil {
 		return nil, err
