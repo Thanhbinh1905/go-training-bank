deed64987db9c3ec77359491beeca6243f1c8006
Thanhbinh1905 <thanhbinh2003195@gmail.com>
2025-09-03 14:22:13 +0000

    add sleep

--- start.sh
+++ start.sh
@@ -5,10 +5,10 @@ set -e
 echo "run db migration"
 source /app/.env
 
+sleep 60s
+
 echo @$DB_SOURCE
 /app/migrate -path /app/migration -database "$DB_SOURCE" -verbose up
-
-sleep 60s
 
 echo "start app"
 exec "$@"
